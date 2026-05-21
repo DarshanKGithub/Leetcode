4b9bf4660b61705a8e6af9ffdef6c96cc5b5913e
Darshan Kshetri <kshetridarshan993@gmail.com>
2026-05-21 14:32:24 +0530

    Space changes

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,41 @@
+package Sorting;
+
+public class InsertionSort {
+    
+    static void insertionSort(int[] arr){
+        int len = arr.length;
+
+        for(int i = 1; i < len; i++){
+
+            int current = arr[i];
+
+            //previous element checl
+            int j = i - 1;
+
+            while( j >= 0 && arr[j] > current){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = current;
+
+
+        }
+    }
+
+    static void print(int[] arr){
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] arr = {2,8,9,0,3,4,5,6};
+        System.out.println("Original Array: ");
+        print(arr);
+        insertionSort(arr);
+        System.out.println("Sorted Array: ");
+        print(arr);
+    }
+}
