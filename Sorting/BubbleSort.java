d4a6c8127de4b6a2d8f7c771490f5b7bd600a5e6
Darshan Kshetri <kshetridarshan993@gmail.com>
2026-05-21 13:19:30 +0530

    Refactor print method in BubbleSort for improved readability

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -17,15 +17,18 @@ public class BubbleSort {
     }
 
     static void print(int[] arr){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for(int val : arr){
+            System.out.print(val + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args){
         int[] arr = {2,8,9,0,3,4,5,6};
+        System.out.println("Original Array: ");
+        print(arr);
         bubbleSort(arr);
+        System.out.println("After sorting Array: ");
         print(arr);
     }
 }
